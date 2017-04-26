2bfc274fdc4addfd72335c19ea50aa32cc4477ea
SEGokberkSercanArslan <se-sercan@outlook.com>
2017-04-26 23:08:33 +0300

    Add files via upload

---
+++ Project_Trafic_Simulator/Class.py
@@ -0,0 +1,12 @@
+
+class roads () :
+    def __init__(self,light_green,light_red):
+        self.red=light_red
+        self.green=light_green
+
+    def set_connection ():
+        pass
+    def set_max_car_number(self):
+        pass
+    def set_max_car_pass(self):
+        pass
